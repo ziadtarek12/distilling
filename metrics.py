39bc72ba6148d31398a3c21d79dfec32831789bc
ziadtarek12 <zi3dtarek@gmail.com>
2025-05-02 22:44:36 +0300

    Fixing eval

--- metrics.py
+++ metrics.py
@@ -39,6 +39,7 @@ def eval_equation(equation):
 def compute_metrics_text(tokenizer):
     def compute_metrics(eval_pred):
         predictions, labels = eval_pred
+        predictions = np.where(predictions != -100, predictions, tokenizer.pad_token_id)
         decoded_preds = tokenizer.batch_decode(predictions[0], skip_special_tokens=True)
 
         labels = np.where(labels[0] != -100, labels[0], tokenizer.pad_token_id)
@@ -54,6 +55,7 @@ def compute_metrics_text(tokenizer):
 def compute_metrics_text_aux(tokenizer):
     def compute_metrics(eval_pred):
         predictions, labels = eval_pred
+        predictions = np.where(predictions != -100, predictions, tokenizer.pad_token_id)
         decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
 
         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
@@ -70,6 +72,7 @@ def compute_metrics_text_aux(tokenizer):
 def compute_metrics_equation(tokenizer):
     def compute_metrics(eval_pred):
         predictions, labels = eval_pred
+        predictions = np.where(predictions != -100, predictions, tokenizer.pad_token_id)
         decoded_preds = tokenizer.batch_decode(predictions[0], skip_special_tokens=True)
 
         labels = np.where(labels[0] != -100, labels[0], tokenizer.pad_token_id)
@@ -93,6 +96,7 @@ def compute_metrics_equation(tokenizer):
 def compute_metrics_equation_aux(tokenizer):
     def compute_metrics(eval_pred):
         predictions, labels = eval_pred
+        predictions = np.where(predictions != -100, predictions, tokenizer.pad_token_id)
         decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
 
         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
